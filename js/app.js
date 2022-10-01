1d1ad4614f0834456441f94107e96acf085d9372
mariamelraggal <mariam.mohamed334@gmail.com>
2022-10-01 23:04:13 +0200

    physical and mental risks pages

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper("header .slide-container .slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
